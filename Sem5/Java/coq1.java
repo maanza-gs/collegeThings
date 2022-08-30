b83429c344827cacd669aeb070ab759cbe1adf37
20PW19@cs2k16.amcs.ws2k12.psgcs.edu <20PW19@cs2k16.amcs.ws2k12.psgcs.edu>
2022-08-30 11:51:15 +0530

    Abstract Class - Java

---
+++ Sem5/Java/coq1.java
@@ -0,0 +1,2 @@
+public class coq1 {
+}
---
+++ Sem5/Java/q1.java
@@ -0,0 +1,48 @@
+class Customer{
+    public:
+        String name;
+        boolean member;
+        String memberType;
+
+        Customer() {
+            name = "";
+            member = false;
+            memberType = "";
+        }
+
+        Customer(String name){
+            this.name = name;
+            member = false;
+            memberType = "";
+        }
+
+        String getName(){
+            return this.name;
+        }
+
+        boolean isMember(){
+            return this.member;
+        }
+
+        void setMember(boolean member){
+            this.member=member;
+        }
+
+        String getMemberType(){
+            return this.memberType;
+        }
+
+        void setMemberType(String type){
+            this.memberType = type;
+        }
+}
+
+public class CObject{
+    public static void main[String[] args]{
+        Scanner s = new Scanner(System.in);
+        Customer c = new Customer("Mac");
+        c.setMember(true);
+        c.memberType("Premium");
+        System.out.print("Member Type: " + c.getMemberType());
+    }
+}
