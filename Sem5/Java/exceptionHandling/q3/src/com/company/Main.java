709532554ebcafce917023bf93c0ea0201cc3775
unknown <20PW19@cs2k16.amcs.ws2k12.psgcs.edu>
2022-09-21 10:09:29 +0530

    ML

--- Sem5/Java/exceptionHandling/q3/src/com/company/Main.java
+++ Sem5/Java/exceptionHandling/q3/src/com/company/Main.java
@@ -6,3 +6,19 @@ public class Main {
 	// write your code here
     }
 }
+
+class contactDetail{
+    private String mobile;
+    private String alternateMobile;
+    private String landline;
+    private String email;
+    private String address;
+
+    contactDetail(){
+
+    }
+
+    public String toString(){
+        return "Mobile: "
+    }
+}
---
+++ Sem5/ML/naivebayes.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("Z:/collegeThings/Sem5/ML/ogbcancer.csv")
+
+train, test = train_test_split(data, test_size=0.2)
+
+n = len(data)
+
+#Finding Prior Probabilty
+m=b=0
+for i in data['class']:
+    if i == 2:
+        m+=1
+    else:
+        b+=1
+        
+pm = m/n
+pb = b/n
+
+features = list(data.columns)
+features.remove('class')
+
+print(features)
+for i in features:
+    freqtable = pd.crosstab(index=train['class'], columns=train[i], margins=True)
+    print(freqtable)
+    
+    
+print(round(freqtable.iloc[1, 0]/ freqtable.iloc[2, 0],3))
