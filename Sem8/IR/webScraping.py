3c90aece70d9cdee7cbac0c94a33af15ebd3835b
unknown <20PW19@cs2k16.amcs.ws2k12.psgcs.edu>
2023-12-26 11:02:08 +0530

    completed web scraping - IR Lab

--- Sem8/IR/webScraping.py
+++ Sem8/IR/webScraping.py
@@ -8,6 +8,7 @@ Created on Tue Dec 19 10:48:58 2023
 import requests 
 from bs4 import BeautifulSoup 
 import lxml
+import pandas as pd
 
 URL = "https://www.goodreads.com/shelf/show/2023-release"
 r = requests.get(URL) 
@@ -17,7 +18,6 @@ soup = BeautifulSoup(r.text, 'html.parser') # If this line causes an error, run 
 
 bookTitles=[] 
 authorNames=[]
-avgRatings=[]
 bookLink=[] 
    
 bookNameScraping = soup.findAll('a', attrs={'class': 'bookTitle'})
@@ -26,9 +26,20 @@ bookNameScraping = soup.findAll('a', attrs={'class': 'bookTitle'})
     
 authorNameScraping = soup.findAll('a', attrs={'class': 'authorName'})
 # for i in authorNameScraping:
-#     print('A: ', i.text)
+#     print('Author: ', i.text)
 
-bookLinkScraping = soup.findAll('a',href=True, )
-for i in bookNameScraping:
-    print('L: ', i,type(i))
+# for i in bookNameScraping:
+#     print('Book:',i.text)
+#     print("link:: ", i.get('href'), '\n')
+
+for i in range(0,len(bookNameScraping)):
+    bookTitles.append(bookNameScraping[i].text)
+    authorNames.append(authorNameScraping[i].text)
+    bookLink.append(bookNameScraping[i].get('href'))
+    # print( bookNameScraping[i].text, '\t',authorNameScraping[i].text ,'\t', bookNameScraping[i].get('href'), '\n')
     
+books = {'name': bookTitles, 'author': authorNames, 'link': bookLink}    
+df = pd.DataFrame(books)
+
+print(df.to_string())
+   
