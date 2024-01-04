97ed39474e7f239cb9f2d356726460e39575267b
unknown <20PW19@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-04 14:37:53 +0530

    Data visualisation lab

--- Sem8/IR/plagiarismDetector2.py
+++ Sem8/IR/plagiarismDetector2.py
@@ -15,70 +15,47 @@ document = {
 
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from functools import reduce
  
 ps = PorterStemmer()
 
-words = word_tokenize(document)
+words = [word_tokenize(i) for i in inputData]
+stemmed_sentence = [reduce(lambda x, y: x + " " + ps.stem(y), i, "") for i in words]
  
-# using reduce to apply stemmer to each word and join them back into a string
-stemmed_sentence = reduce(lambda x, y: x + " " + ps.stem(y), words, "")
- 
-print(stemmed_sentence)
+# print(stemmed_sentence)
+
+tokenized = [ word_tokenize(c) for c in inputData ]
+#print(tokenized)
+
+stop_words = stopwords.words('english')
+
+l = []
+
+for i in words:
+    s= ' '.join(i)
+    token = word_tokenize(s)
+    cleaned_token = []
+    for word in token:
+        if word not in stop_words:
+            cleaned_token.append(word)
+    print(cleaned_token)        
+    l.append(cleaned_token)
+    stemmed = [ps.stem(word) for word in cleaned_token]
+    print(stemmed)
 
 
+# class PlagiarismChecker:
+#   def binary_distance(self, u, v):
+#         # Binary distance between vectors u and v
+#         return int(u != v)
 
-# from collections import Counter
-# from tqdm import tqdm
-# from scipy.sparse import csr_matrix
-# import math
-# import operator
-# from sklearn.preprocessing import normalize
-# import numpy
-
-# def IDF(document, unique_words):
-#     idf_vals = {}    
-#     total_docs = len(document) 
-#     for word in unique_words: 
-#         cnt = 0
-#         for row in document:
-#             if word in row.split(" "): 
-#                 cnt+=1 
-#         idf_vals[word] = 1 + math.log((1+total_docs)/(1+cnt)) 
-#     return idf_vals
-
-# def fit(dataset):    
-#     unique_words = set() 
-#     if isinstance(dataset, (list,)):
-#         print("Unique Words: ",unique_words)
-#         for row in dataset:
-#             for word in row.split(" "):
-#                 if len(word) < 2:
-#                     continue
-#                 unique_words.add(word)
-#         unique_words = sorted(list(unique_words))
-#         print("Unique Words: ",unique_words)
-#         vocab = {j:i for i,j in enumerate(unique_words)}
-#     # idfs_ = IDF(dataset, unique_words)
-#     # return vocab, idfs_
-
-# # features, idfs_ = fit(document)
-# fit(document)
-
-# def transform(dataset):
-#     sparse_matrix = csr_matrix((len(dataset), len(features)), dtype=float)
-#     for row in range(0, len(dataset)):
-#         word_count = Counter(dataset[row].split(' '))
-#         for word in dataset[row].split(' '):
-#             if word in list(features.keys()):
-#                 tf = word_count[word] / len(dataset[row].split(' '))
-#                 tfidf = tf * idfs_[word]
-#                 sparse_matrix[row, features[word]] = tfidf
-#     output = normalize(sparse_matrix, norm='l2', axis = 1, copy=True, return_norm=False)
-#     return output
-
-# tfidf_corpus = transform(document)
-
-# idf_list = list(idfs_.values())
-# print("Our idfs: ", idf_list) #This will give us idf vals calc by our custom implementation
-# print(tfidf_corpus[0].toarray())  #This will give us tfidf vals for the first document calc by our custom implementation
+#   def check_duplicate_title(self, new_title):
+#         for existing_title in self.documents:
+#             distance = self.binary_distance(new_title, existing_title)
+#             if distance == 0:
+#                 print("Document discarded as it has a duplicate title.")
+#                 return True
+#         return False
+    
+    
